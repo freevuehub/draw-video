e9a7ddcdc54b6b0b7821fc5dcf6dcd92a3d63c53
홍성준 <freevue@hongseongjun-ui-MacBookPro.local>
2020-05-28 10:36:44 +0900

    [refactor]: 불필요 리소스 제거

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as DrawCanvas } from './DrawCanvas.jsx'
-export { default as DefaultVideo } from './DefaultVideo.jsx'
 export { default as Result } from './Result.jsx'
 export { default as Cursor } from './Cursor.jsx'
 export { default as Video } from './Video.jsx'
